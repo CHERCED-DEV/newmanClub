6a2e334a6cb15adfa62e69da8e17a4141d88100f
Camilo <hitmancodeme47@hotmail.com>
2023-04-29 16:50:56 -0500

    last for theday

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,18 @@
 import type { AppProps } from 'next/app'
 import "../sass/styles.scss"
-import { lazy, useCallback, useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { getCMSData } from '../../utils/handlers/requests';
 
 const Layout = lazy(() => import("../components/ui-kit/Layout"));
 
 export default function App({ Component, pageProps }: AppProps) {
 
-    const storageConstructor = useCallback(async () => {
-        const CmsData = await getCMSData();
-        window.localStorage.setItem("cmsData", JSON.stringify(CmsData));
-    }, []);
-
     useEffect(() => {
-        storageConstructor()
-    }, [storageConstructor])
+        (async () => {
+            const CmsData = await getCMSData();
+            window.localStorage.setItem("cmsData", JSON.stringify(CmsData));
+        })
+    }, [])
 
     return (
         <Layout>
