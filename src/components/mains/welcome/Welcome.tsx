57582539b88a7794fc0d74d7284eda2553b07c75
Camilo <hitmancodeme47@hotmail.com>
2023-04-29 15:13:10 -0500

    load video lick resolved

--- src/components/mains/welcome/Welcome.tsx
+++ src/components/mains/welcome/Welcome.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useRef, useEffect, memo, lazy } from 'react';
 import Image from 'next/image';
-import { WelcomeDataProps } from './utils/welcome.interface';
+import { WelcomeDataProps, WelcomeConfig } from './utils/welcome.interface';
 
 const JoinUs = lazy(() => import("./utils/JoinUs"));
 
 const Welcome: React.FC<WelcomeDataProps> = ({ welcome }) => {
-    const [playing, setPlaying] = useState(true);
+    const [playing, setPlaying] = useState<boolean>(true);
     const [wideScreen, setWideScreen] = useState(window.innerWidth >= 640);
+    const [videoLoaded, setVideoLoaded] = useState<boolean>(false)
     const videoRef = useRef<HTMLVideoElement>(null);
 
     const togglePlay = () => {
@@ -30,43 +31,78 @@ const Welcome: React.FC<WelcomeDataProps> = ({ welcome }) => {
         };
     }, []);
 
-    if(welcome) {
-        return (
-            <>
-                <section className="welcome">
-                    {wideScreen ? (
-                        <video ref={videoRef} autoPlay={true} muted={true} playsInline={true} className="welcome__video">
-                            <source src={welcome.video.dsk.src} type="video/mp4" />
-                            {welcome.video.dsk.alert}<code>video</code>.
-                        </video>
-                    ) : (
-                        <video ref={videoRef} autoPlay={true} muted={true} playsInline={true} className="welcome__video">
-                            <source src={welcome.video.mob.src} type="video/mp4" />
-                            {welcome.video.mob.alert}<code>video</code>.
-                        </video>
-                    )}
-                    <button onClick={togglePlay} className="welcome__play">
-                        {playing ? (
-                            <Image
-                                src={welcome.button.onPlay.src}
-                                alt={welcome.button.onPlay.alt}
-                                width={welcome.button.onPlay.width}
-                                height={welcome.button.onPlay.height}
-                            />
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <>
+            {
+                welcome ? (
+                    <section className="welcome">
+                        {wideScreen ? (
+                            <video
+                                ref={videoRef}
+                                autoPlay={true}
+                                muted={true}
+                                playsInline={true}
+                                onLoadedData={() => setVideoLoaded(true)}
+                                className="welcome__video">
+                                <source src={welcome.video.dsk.src} type="video/mp4" />
+                                {welcome.video.dsk.alert}<code>video</code>.
+                            </video>
                         ) : (
-                            <Image
-                                src={welcome.button.onPause.src}
-                                alt={welcome.button.onPause.alt}
-                                width={welcome.button.onPause.width}
-                                height={welcome.button.onPause.height}
-                            />
+                            <video
+                                ref={videoRef}
+                                autoPlay={true}
+                                muted={true}
+                                playsInline={true}
+                                onLoadedData={() => setVideoLoaded(true)}
+                                className="welcome__video">
+                                <source src={welcome.video.mob.src} type="video/mp4" />
+                                {welcome.video.mob.alert}<code>video</code>.
+                            </video>
                         )}
-                    </button>
-                </section>
-                <JoinUs join_us={welcome.join_us} />
-            </>
-        );
-    }
+                        {videoLoaded ? null : (
+                            <>
+                                {
+                                    wideScreen ? (
+                                        <div className='welcome__bg-sk'>
+                                        </div>
+                                    ) : (
+                                        <div className='welcome__bg-mb'>
+                                        </div>
+                                    )
+                                }
+                            </>
+                        )}
+                        <button onClick={togglePlay} className="welcome__play">
+                            {playing ? (
+                                <Image
+                                    src={welcome.button.onPlay.src}
+                                    alt={welcome.button.onPlay.alt}
+                                    width={welcome.button.onPlay.width}
+                                    height={welcome.button.onPlay.height}
+                                />
+                            ) : (
+                                <Image
+                                    src={welcome.button.onPause.src}
+                                    alt={welcome.button.onPause.alt}
+                                    width={welcome.button.onPause.width}
+                                    height={welcome.button.onPause.height}
+                                />
+                            )}
+                        </button>
+                    </section>
+                ) : (
+                    <section className="welcome">
+
+                    </section>
+                )
+            }
+            <JoinUs join_us={welcome.join_us} />
+        </>
+    );
 };
 
 export default memo(Welcome);
