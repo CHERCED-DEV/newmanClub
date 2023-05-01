ec77326bf777a10a721f804ad366f9ca501f8331
Camilo <hitmancodeme47@hotmail.com>
2023-05-01 17:57:59 -0500

    grid for memores

--- src/components/mains/group-options/Memories.tsx
+++ src/components/mains/group-options/Memories.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { MemoriesDataProps } from './utils/groupoptions.interface'
 import Image from 'next/image'
 
 const Memories: React.FC<MemoriesDataProps> = ({ memories }) => {
     return (
         <>
-        <section className='memories'>
-            <header className='memories__header'>
-                <Image
-                    src={memories.main_logo.src}
-                    alt={memories.main_logo.alt}
-                    width={memories.main_logo.width}
-                    height={memories.main_logo.height}
-                />
-                <h2 className='memories__header-title'>{memories.title}</h2>
-            </header>
-            <ul className='memories__list'>
-                <li className="memories__item">
+            <section className='memories'>
+                <header className='memories__header'>
+                    <Image
+                        src={memories.main_logo.src}
+                        alt={memories.main_logo.alt}
+                        width={memories.main_logo.width}
+                        height={memories.main_logo.height}
+                    />
+                    <h2 className='memories__header-title'>{memories.title}</h2>
+                </header>
+                <ul className='memories__list'>
+
                     {
                         memories.cards?.map((card) => (
-                            <Image
-                                key={card.src}
-                                src={card.src}
-                                alt={card.alt}
-                                width={card.width}
-                                height={card.height}
-                            />
-
+                            <li key={card.src} className="memories__item">
+                                <Image
+                                    src={card.src}
+                                    alt={card.alt}
+                                    width={card.width}
+                                    height={card.height}
+                                />
+                            </li>
                         ))
                     }
-                </li>
-            </ul>
-            <footer className="memories__footer">
-                <button className="memories__button">{memories.button}</button>
-            </footer>
-        </section>
+
+                </ul>
+                <footer className="memories__footer">
+                    <button className="memories__button">{memories.button}</button>
+                </footer>
+            </section>
         </>
-        
+
     )
 }
 
-export default React.memo(Memories)
+export default memo(Memories)
